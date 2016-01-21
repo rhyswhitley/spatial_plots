b94d09e5351a3c83d7c49aad3b5efc4d7d707942
Rhys Whitley <rhys.whitley@gmail.com>
2016-01-22 10:04:42 +1100

    clipping now works correctly, also cleaned up the script a bit

--- src/main.py
+++ src/main.py
@@ -5,24 +5,17 @@ from mpl_toolkits.basemap import Basemap
 from matplotlib.cm import get_cmap
 from matplotlib.path import Path
 from matplotlib.patches import PathPatch
-from matplotlib.collections import PatchCollection
 
-from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon
-from shapely.prepared import prep
-
-#from matplotlib.mlab import griddata as griddata2
-#from pylab import meshgrid
+from shapely.geometry import shape, Point, Polygon, MultiPoint, MultiPolygon
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import pandas as pd
 import numpy as np
 import datetime, time
-import shapefile
 import os, re
 
 import fiona
-#import colormaps as cmaps
 
 
 __author__ = 'Rhys Whitley'
@@ -72,16 +65,11 @@ def import_data(fname, doSum=False):
     # return to user as a dict
     return pd.DataFrame({'lon':id_lons, 'lat':id_lats, 'data':id_clim})
 
-def down_sample(mapObj, _lons, _lats, _data, samp_size=1e4):
-
-    print(mapObj.xmax)
-    print(mapObj.xmin)
-    print(mapObj.ymax)
-    print(mapObj.ymin)
+def grid_resample(mapObj, _lons, _lats, _data, res=0.05):
 
     # create x, y complex dimensions for creating the new grid
-    nx = complex(0, int((mapObj.xmax - mapObj.xmin)/samp_size))
-    ny = complex(0, int((mapObj.ymax - mapObj.ymin)/samp_size))
+    nx = complex(0, int((mapObj.xmax - mapObj.xmin)/res))
+    ny = complex(0, int((mapObj.ymax - mapObj.ymin)/res))
 
     # echo to user
     msg = 'resolution is {0} x {1}'.format(nx, ny)
@@ -97,6 +85,38 @@ def down_sample(mapObj, _lons, _lats, _data, samp_size=1e4):
     # return the new grid
     return xl, yl, ds_zi
 
+
+def define_clipping(_shapePath, *args, **kwargs):
+
+    # import the shapefile using fiona
+    fshape = fiona.open(_shapePath)
+
+    # extract the vertices of the polygon (the coord system) **weirdly stored
+    # as a list of lists
+    vert_2Dlist = [vl[0] for vl in fshape.next()["geometry"]["coordinates"]]
+    # flatten 2D list
+    vert_1Dlist = list_flat(vert_2Dlist)
+
+    # define the path by which the lines of the polygon are drawn
+    code_2Dlist = [create_codes(len(vl)) for vl in vert_2Dlist]
+    # flatten 2D list
+    code_1Dlist = list_flat(code_2Dlist)
+
+    # create the art path that will clip the data (Multipolygons are flattened)
+    clip = PathPatch(Path(vert_1Dlist, code_1Dlist), *args, **kwargs)
+
+    # create a multi-polygon object using the same list of coordinates
+    mpoly = MultiPolygon([shape(vl["geometry"]) for vl in fshape])
+
+    # return to user
+    return {'patch': clip, 'polygon': mpoly}
+
+def list_flat(List2D):
+    return [item for sublist in List2D for item in sublist]
+
+def create_codes(plen):
+    return [Path.MOVETO] + [Path.LINETO]*(plen-2) + [Path.CLOSEPOLY]
+
 def paint_map(ax_0, dataset, levels):
 
     if (lonWest < 0) and (lonEast < 0):
@@ -106,54 +126,46 @@ def paint_map(ax_0, dataset, levels):
     else:
         lon_0 = (lonEast + lonWest)/2.0
 
+    # create a Basemap canvas to plot the data on [NOTE: be aware of projection geo-coordinate system]
     oz_map = Basemap(llcrnrlon=lonWest, llcrnrlat=latSouth, urcrnrlon=lonEast, urcrnrlat=latNorth, \
              resolution='i', projection='cyl', lat_0=latNorth, lon_0=lon_0, ax=ax_0)
+
+    # draw spatial extras to denote land and sea
     oz_map.drawmapboundary(fill_color='dimgray')
     oz_map.drawcoastlines(color='black', linewidth=0.5)
-    oz_map.fillcontinents(color='white', lake_color='dimgray', zorder=0)
+    oz_map.fillcontinents(color='lightgray', lake_color='dimgray', zorder=0)
     # draw parallels and meridians.
     oz_map.drawparallels(np.arange(-80, 90, 5), color='grey', labels=[1, 0, 0, 0])
     oz_map.drawmeridians(np.arange(0, 360, 5), color='grey', labels=[0, 0, 0, 1])
 
-    lx, ly, zi = down_sample(oz_map, dataset['lon'], dataset['lat'], dataset['data'], 0.05)
+    # draw gridded data onto the map canvas
+    lx, ly, zi = grid_resample(oz_map, dataset['lon'], dataset['lat'], dataset['data'], 0.05)
     x, y = oz_map(lx, ly)
-    cs = oz_map.contourf(x, y, zi, levels, cmap=get_cmap(MAPCOLOR, 100))
+    #cs = oz_map.contourf(x, y, zi, levels, cmap=get_cmap(MAPCOLOR, 100))
 
-    # add savanna bioregion polygon
-    bio_file = os.path.splitext(SHAPEPATH)[0]
-    sf = shapefile.Reader(bio_file)
+    # import savanna bioregion polygon and create a clipping region
+    sav_geom = define_clipping(SHAPEPATH, transform=ax_0.transData)
 
-    for shape_rec in sf.shapeRecords():
-        vertices = []
-        codes = []
-        pts = shape_rec.shape.points
-        prt = list(shape_rec.shape.parts) + [len(pts)]
-        for i in range(len(prt) - 1):
-            for j in range(prt[i], prt[i+1]):
-                vertices.append((pts[j][0], pts[j][1]))
-            codes += [Path.MOVETO]
-            codes += [Path.LINETO] * (prt[i+1] - prt[i] -2)
-            codes += [Path.CLOSEPOLY]
-        clip = PathPatch(Path(vertices, codes), edgecolor='k', facecolor=None, transform=ax_0.transData)
 
-    # Rasterize the contour collections
-    plt.gca().add_patch(clip)
-    for contour in cs.collections:
-        contour.set_clip_path(clip)
-        #contour.set_rasterized(True)
+    print(zi)
+    mask = np.zeros((len(x), len(y)))
+    for i, p_lon in enumerate(x):
+        for j, p_lat in enumerate(y):
+            test = sav_geom['polygon'].contains(Point(x, y))
+            if test is True:
+                mask[i, j] = 1
+
+    #cs = oz_map.contourf(x, y, mask, levels, cmap=get_cmap(MAPCOLOR, 100))
+
+
+    # Clip and Rasterize the contour collections
+#    for contour in cs.collections:
+#        contour.set_clip_path(sav_geom['patch'])
+#        #contour.set_rasterized(True)
 
     return None
 
 def plot_map():
-
-    #shape = shapefile.Reader(os.path.splitext(SHAPEPATH)[0])
-#    shape = fiona.open(SHAPEPATH)
-#    pol = shape.next()
-#    geom = pol['geometry']
-#    poly_data = pol["geometry"]["coordinates"]
-#    poly_data2 = poly_data[len(poly_data)-1][0]
-#    poly = Polygon(poly_data2)
-
 
     tair = import_data("anuclim_5km_mat.csv", doSum=False)
     #rain = import_data("anuclim_5km_ppt.csv", doSum=True)
@@ -196,13 +208,12 @@ if __name__ == '__main__':
 
 
 
-#    # add savanna bioregion polygon
-#    bio_file = os.path.splitext(SHAPEPATH)[0]
-##    bio_name = os.path.basename(bio_file)
-##    sf = oz_map.readshapefile(bio_file, bio_name)
-#    sf = shapefile.Reader(bio_file)
-#
-#    for shape_rec in sf.shapeRecords():
+#def shape_to_Patch(shape_obj):
+#    """
+#    This is a temporary solution to create a Patch object that is used to clip
+#    the Basemap object for plotting data bounded by a shapefile.
+#    """
+#    for shape_rec in shape_obj.shapeRecords():
 #        vertices = []
 #        codes = []
 #        pts = shape_rec.shape.points
@@ -213,46 +224,8 @@ if __name__ == '__main__':
 #            codes += [Path.MOVETO]
 #            codes += [Path.LINETO] * (prt[i+1] - prt[i] -2)
 #            codes += [Path.CLOSEPOLY]
-#        clip = PathPatch(Path(vertices, codes), transform=ax_0.transData)
 #
-#    print(type(clip))
-#    return 1
-
-#    verts = [
-#        (130, -30), # left, bottom
-#        (130, -20), # left, top
-#        (140, -20), # right, top
-#        (140, -30), # right, bottom
-#        (130, -30) # ignored
-#        ]
+#        clip = PathPatch(Path(vertices, codes), edgecolor='k', facecolor=None, transform=ax_0.transData)
 #
-#    codes = [Path.MOVETO,
-#            Path.LINETO,
-#            Path.LINETO,
-#            Path.LINETO,
-#            Path.CLOSEPOLY
-#            ]
+#        return clip
 #
-#    path = Path(verts, codes)
-#    print(path)
-#    patch = PathPatch(path, transform=ax_0.transData)
-##    shp_info = oz_map.readshapefile(os.path.splitext(SHAPEPATH)[0], 'savanna', drawbounds=True)
-#    #print(shp_info[-1]._paths[0].vertices)
-#    paths = []
-#    for line in shp_info[-1]._paths:
-#        moves = len(line.vertices) - 2
-#        my_codes = [Path.MOVETO] + [Path.LINETO]*moves + [Path.CLOSEPOLY]
-#        my_verts = [(vx, vy) for (vx, vy) in line.vertices]
-#        paths.append(Path(line.vertices, my_codes))
-#
-#    print(paths[0])
-#    coll = PatchCollection(paths, linewidths=1, facecolors='pink', zorder=2)
-#    ax_0.add_collection(patch)
-
-#    for shape in oz_map.savanna:
-#        print(len(shape))
-#        patches.append(Polygon(np.array(shape), True))
-
-    #ax_0.add_collection(PatchCollection(poly, facecolor='m', edgecolor='k', linewidths=1., zorder=2))
-
-
