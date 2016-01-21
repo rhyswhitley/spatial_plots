6176a3e1e3a5c84fc081383caadeabbdd6d6570b
Rhys Whitley <rhys.whitley@gmail.com>
2016-01-22 10:27:46 +1100

    added more comments (needs to be better)

--- src/main.py
+++ src/main.py
@@ -65,14 +65,23 @@ def import_data(fname, doSum=False):
     # return to user as a dict
     return pd.DataFrame({'lon':id_lons, 'lat':id_lats, 'data':id_clim})
 
-def grid_resample(mapObj, _lons, _lats, _data, res=0.05):
+def grid_resample(mapObj, _lons, _lats, _data, proj_res=0.05):
+    """
+    Resamples (or doesn't depending on what you pass) the gridded data to a new
+    resolution. Resolution is defined by the proj_res parameter, which is the
+    size of each grid cell in the maps units of projection.
+
+    Currently works out the new grid based on the basemap object extents. Not
+    sure this is the best way, and perhaps doing this independent of the
+    basemap projection would be better?
+    """
 
     # create x, y complex dimensions for creating the new grid
-    nx = complex(0, int((mapObj.xmax - mapObj.xmin)/res))
-    ny = complex(0, int((mapObj.ymax - mapObj.ymin)/res))
+    nx = complex(0, int((mapObj.xmax - mapObj.xmin)/proj_res))
+    ny = complex(0, int((mapObj.ymax - mapObj.ymin)/proj_res))
 
     # echo to user
-    msg = 'resolution is {0} x {1}'.format(nx, ny)
+    msg = 'Map resolution is {0} x {1}'.format(nx, ny)
     print(msg)
 
     # define the grid space based on the new dimensions
@@ -85,8 +94,15 @@ def grid_resample(mapObj, _lons, _lats, _data, res=0.05):
     # return the new grid
     return xl, yl, ds_zi
 
-
 def define_clipping(_shapePath, *args, **kwargs):
+    """
+    Reads in a shapefile from some folder and creates a Matplotlib Patch artist
+    from which one can clip gridded data plotted on a basemap object. The Patch
+    object is defined using vertices (lat/lon coordinates) and codes (drawing
+    commands), which make up the final PatchPath that is returned to the user.
+    Additionally, a polygon object is also created to extract data points from
+    a gridded dataset that exist with the polygon's extents.
+    """
 
     # import the shapefile using fiona
     fshape = fiona.open(_shapePath)
@@ -112,12 +128,30 @@ def define_clipping(_shapePath, *args, **kwargs):
     return {'patch': clip, 'polygon': mpoly}
 
 def list_flat(List2D):
+    """Flattens a 2D list"""
     return [item for sublist in List2D for item in sublist]
 
 def create_codes(plen):
+    """
+    Returns a list of matplotlib artist drawing codes based on the number of
+    polygon coordinates; First index is the starting point, Last index closes
+    the polygon, and all other indices draw the polygon (coordinates always
+    loop back to origin)
+    """
     return [Path.MOVETO] + [Path.LINETO]*(plen-2) + [Path.CLOSEPOLY]
 
 def paint_map(ax_0, dataset, levels):
+    """
+    Creates a plot canvas using basemap to visualise geospatial data, which is
+    passed to this function as 'dataset'. Extents of the map are based on the
+    global settings at the bottom of the file [need to change this to calculate
+    extents from the imported data].
+
+    The projection used here is cylindrical, although you could change this to
+    anything else, but be warned, this will created problems with the clipping,
+    which is basic lat/lon. If you change the projection, the clipping projection
+    must also be changed [not currently automatic].
+    """
 
     if (lonWest < 0) and (lonEast < 0):
         lon_0 = -(abs(lonEast) + abs(lonWest))/2.0
@@ -126,46 +160,45 @@ def paint_map(ax_0, dataset, levels):
     else:
         lon_0 = (lonEast + lonWest)/2.0
 
-    # create a Basemap canvas to plot the data on [NOTE: be aware of projection geo-coordinate system]
-    oz_map = Basemap(llcrnrlon=lonWest, llcrnrlat=latSouth, urcrnrlon=lonEast, urcrnrlat=latNorth, \
-             resolution='i', projection='cyl', lat_0=latNorth, lon_0=lon_0, ax=ax_0)
+    # create a Basemap canvas to plot the data on [NOTE: be aware of projection
+    # geo-coordinate system]
+    oz_map = Basemap(llcrnrlon=lonWest, llcrnrlat=latSouth,
+                     urcrnrlon=lonEast, urcrnrlat=latNorth, \
+                     resolution='i', projection='cyl', \
+                     lat_0=latNorth, lon_0=lon_0, ax=ax_0)
 
     # draw spatial extras to denote land and sea
     oz_map.drawmapboundary(fill_color='dimgray')
     oz_map.drawcoastlines(color='black', linewidth=0.5)
     oz_map.fillcontinents(color='lightgray', lake_color='dimgray', zorder=0)
+
     # draw parallels and meridians.
     oz_map.drawparallels(np.arange(-80, 90, 5), color='grey', labels=[1, 0, 0, 0])
     oz_map.drawmeridians(np.arange(0, 360, 5), color='grey', labels=[0, 0, 0, 1])
 
     # draw gridded data onto the map canvas
-    lx, ly, zi = grid_resample(oz_map, dataset['lon'], dataset['lat'], dataset['data'], 0.05)
+    lx, ly, zi = grid_resample(oz_map, dataset['lon'], dataset['lat'], \
+                               dataset['data'], res=0.05)
     x, y = oz_map(lx, ly)
-    #cs = oz_map.contourf(x, y, zi, levels, cmap=get_cmap(MAPCOLOR, 100))
+    cs = oz_map.contourf(x, y, zi, levels, cmap=get_cmap(MAPCOLOR, 100))
 
     # import savanna bioregion polygon and create a clipping region
     sav_geom = define_clipping(SHAPEPATH, transform=ax_0.transData)
 
-
-    print(zi)
-    mask = np.zeros((len(x), len(y)))
-    for i, p_lon in enumerate(x):
-        for j, p_lat in enumerate(y):
-            test = sav_geom['polygon'].contains(Point(x, y))
-            if test is True:
-                mask[i, j] = 1
-
-    #cs = oz_map.contourf(x, y, mask, levels, cmap=get_cmap(MAPCOLOR, 100))
-
-
     # Clip and Rasterize the contour collections
-#    for contour in cs.collections:
-#        contour.set_clip_path(sav_geom['patch'])
-#        #contour.set_rasterized(True)
+    for contour in cs.collections:
+        contour.set_clip_path(sav_geom['patch'])
+        contour.set_rasterized(True)
 
-    return None
+    return oz_map
 
 def plot_map():
+    """
+    Top level function that draws the final figure: could be one or
+    multiple maps.
+
+    Warning: This isn't complete yet.
+    """
 
     tair = import_data("anuclim_5km_mat.csv", doSum=False)
     #rain = import_data("anuclim_5km_ppt.csv", doSum=True)
@@ -197,7 +230,7 @@ if __name__ == '__main__':
     SHAPEPATH = os.path.expanduser("~/Savanna/Data/GiS/Savanna-Boundary-crc-g/crc-g.shp")
 
     MAPCOLOR = 'GnBu'
-    #MAPCOLOR = 'jet'
+    #MAPCOLOR = 'Viridis' # need to register
 
     latNorth = -5
     latSouth = -30
@@ -207,6 +240,7 @@ if __name__ == '__main__':
     plot_map()
 
 
+# JUNK BELOW
 
 #def shape_to_Patch(shape_obj):
 #    """
@@ -228,4 +262,12 @@ if __name__ == '__main__':
 #        clip = PathPatch(Path(vertices, codes), edgecolor='k', facecolor=None, transform=ax_0.transData)
 #
 #        return clip
+#
+#    print(zi)
+#    mask = np.zeros((len(x), len(y)))
+#    for i, p_lon in enumerate(x):
+#        for j, p_lat in enumerate(y):
+#            test = sav_geom['polygon'].contains(Point(x, y))
+#            if test is True:
+#                mask[i, j] = 1
 #
